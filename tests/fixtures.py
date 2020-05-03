9a07982481c6ecec637b47a02d1d74f8c36baa4a
guiferviz <guiferviz@gmail.com>
2020-05-03 20:34:12 +0200

    Adding some Mocked transformations to test the pipeline.

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,5 +1,11 @@
 
+import time
+
 import pandas as pd
+
+from sklearn.base import BaseEstimator, TransformerMixin
+
+from recipipe.core import RecipipeTransformer
 
 
 def create_df_cat():
@@ -29,3 +35,68 @@ def create_df_all():
         create_df_float(),
         create_df_int()
     ], axis=1)
+
+
+class TransformerMock:
+    """Mock implementing the Transformer API without inheritance. """
+
+    def __init__(self):
+        self.n_fit = 0
+        self.n_transform = 0
+        self.n_inverse_transform = 0
+
+    def fit(self, X, y):
+        self.n_fit += 1
+        return self
+
+    def transform(self, X):
+        self.n_transform += 1
+        return X
+
+    def inverse_transform(self, X):
+        self.n_inverse_transform += 1
+        return X
+
+
+class SklearnTransformerMock(BaseEstimator, TransformerMixin):
+    """Mock implementing the SKLearn APIs with inheritance. """
+
+    def __init__(self):
+        self.n_fit = 0
+        self.n_transform = 0
+        self.n_inverse_transform = 0
+
+    def fit(self, X, y):
+        self.n_fit += 1
+        return self
+
+    def transform(self, X):
+        self.n_transform += 1
+        return X
+
+    def inverse_transform(self, X):
+        self.n_inverse_transform += 1
+        return X
+
+
+class RecipipeTransformerMock(RecipipeTransformer):
+    """Mock implementing RecipipeTransformer API with inheritance. """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.n_fit = 0
+        self.n_transform = 0
+        self.n_inverse_transform = 0
+
+    def _fit(self, X, y):
+        self.n_fit += 1
+        return self
+
+    def _transform(self, X):
+        self.n_transform += 1
+        return X
+
+    def _inverse_transform(self, X):
+        self.n_inverse_transform += 1
+        return X
+
